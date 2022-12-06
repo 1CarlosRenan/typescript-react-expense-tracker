07affb52512433afa1642484799fc7faf4766d5c
1CarlosRenan <carlosrenan96@gmail.com>
2022-12-05 22:48:49 -0300

    add previous and next month functions

--- src/App.tsx
+++ src/App.tsx
@@ -17,13 +17,20 @@ const App = () => {
     setFilteredList(filterListByMonth(list, currentMonth));
   }, [list, currentMonth]);
 
+  const handleMonthChange = (newMonth: string) => {
+    setCurrentMonth(newMonth)
+  }
+
   return (
     <C.Container>
       <C.Header>
         <C.HeaderText>Sistema Financeiro</C.HeaderText>
       </C.Header>
       <C.Body>
-        <InfoArea currentMonth={currentMonth} />
+        <InfoArea
+          currentMonth={currentMonth}
+          onMonthChange={handleMonthChange}
+        />
 
         <TableArea list={filteredList}/>
       </C.Body>
