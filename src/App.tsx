e445e83c0e4665a97cd757bfe1e063052ecd5f32
1CarlosRenan <carlosrenan96@gmail.com>
2023-02-09 22:20:08 -0300

    adding resume items

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,8 @@ const App = () => {
   const [list, setList] = useState(items);
   const [filteredList, setFilteredList] = useState<Item[]>([]);
   const [currentMonth, setCurrentMonth] = useState(getCurrentMonth());
+  const [income, setIncome] = useState(0);
+  const [expense, setExpense] = useState(0);
 
   useEffect(() => {
     setFilteredList(filterListByMonth(list, currentMonth));
@@ -30,6 +32,8 @@ const App = () => {
         <InfoArea
           currentMonth={currentMonth}
           onMonthChange={handleMonthChange}
+          income={income}
+          expense={expense}
         />
 
         <TableArea list={filteredList}/>
