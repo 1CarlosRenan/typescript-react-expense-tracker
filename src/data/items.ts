bda4dfcf41cf931a3a7cf33e26292ea662b0ccc5
1CarlosRenan <carlosrenan96@gmail.com>
2023-02-15 21:10:19 -0300

    updating data to test

--- src/data/items.ts
+++ src/data/items.ts
@@ -2,25 +2,25 @@ import { Item } from "../types/Item";
 
 export const items: Item[] = [
   {
-    date: new Date(2021, 9, 15),
+    date: new Date(2023, 1, 15),
     category: "food",
     title: "McDonalds",
     value: 32.12,
   },
   {
-    date: new Date(2021, 9, 15),
+    date: new Date(2023, 1, 15),
     category: "food",
     title: "Burger King",
     value: 28,
   },
   {
-    date: new Date(2021, 9, 16),
+    date: new Date(2023, 1, 16),
     category: "rent",
     title: "Aluguel Apt",
     value: 2300,
   },
   {
-    date: new Date(2021, 10, 15),
+    date: new Date(2023, 2, 15),
     category: "salary",
     title: "Salário ACME",
     value: 4500,
